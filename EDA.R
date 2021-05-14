09cbf824e9d99f95dc206db1543db841bb043579
danielden19 <74244213+danielden19@users.noreply.github.com>
2021-05-14 15:51:16 +0100

    Created a function to simulate the hidden Markov model Adjusted EDA to give appropriate graphics

--- EDA.R
+++ EDA.R
@@ -52,8 +52,39 @@ ggplot(data = data) +
   geom_boxplot(mapping = aes(x = factor(Day, level = level_order), y = NE_DM)) + 
   xlab("")
 
-## Create scatter plots of relationships within variables (CWV and DM)
-ggplot(data = data) + geom_point(mapping = aes(x = NO_CWV, y = NE_CWV), size = 0.5)
+## Line graphs showing demand with points over the top for bank holidays
+ggplot(data = data, mapping = aes(x = Date, y = NO_DM)) + 
+  geom_line(data = data[which(data$Bank.Holiday==FALSE),], aes(colour = factor(Bank.Holiday)), lwd = 0.35) + 
+  geom_point(data = data[which(data$Bank.Holiday==TRUE),], aes(shape = factor(Bank.Holiday))) + 
+  theme(legend.position = "bottom") + 
+  labs(colour = "legend") + 
+  scale_colour_discrete(name = "", breaks = c("TRUE", "FALSE"), 
+                        labels = c("Bank holiday", "Non-bank holiday"),
+                        type = c("royalblue", "black")) +
+  scale_shape_discrete(name = "", breaks = c("TRUE", "FALSE"), 
+                       labels = c("Bank holiday", "Non-bank holiday"))
+
+ggplot(data = data, mapping = aes(x = Date, y = NE_DM)) + 
+  geom_line(data = data[which(data$Bank.Holiday==FALSE),], aes(colour = factor(Bank.Holiday)), lwd = 0.35) + 
+  geom_point(data = data[which(data$Bank.Holiday==TRUE),], aes(shape = factor(Bank.Holiday))) + 
+  theme(legend.position = "bottom") + 
+  labs(colour = "legend") + 
+  scale_colour_discrete(name = "", breaks = c("TRUE", "FALSE"), 
+                        labels = c("Bank holiday", "Non-bank holiday"),
+                        type = c("royalblue", "black")) +
+  scale_shape_discrete(name = "", breaks = c("TRUE", "FALSE"), 
+                       labels = c("Bank holiday", "Non-bank holiday"))
+
+## Boxplots showing the difference in demand between bank hols and non-bank hols
+ggplot(data = data) + geom_boxplot(mapping = aes(x = Bank.Holiday, y = NO_DM),
+                                   fill = c("royalblue", "darkgrey")) + xlab("") +
+  scale_x_discrete(labels = c("FALSE" = "Non-bank holiday", "TRUE" = "Bank holiday"))
+ggplot(data = data) + geom_boxplot(mapping = aes(x = Bank.Holiday, y = NE_DM),
+                                   fill = c("royalblue", "darkgrey")) + xlab("") + 
+  scale_x_discrete(labels = c("FALSE" = "Non-bank holiday", "TRUE" = "Bank holiday"))
+
+
+## Create scatter plots of relationships within variables (DM)
 ggplot(data = data) + geom_point(mapping = aes(x = NO_DM, y = NE_DM), size = 0.5)
 ## Now within locations (NO and NE)
 ggplot(data = data) + geom_point(mapping = aes(x = NO_CWV, y = NO_DM), size = 0.4)
@@ -62,6 +93,14 @@ ggplot(data = data) + geom_point(mapping = aes(x = NE_CWV, y = NE_DM), size = 0.
 ## Perform K-means on these scatter plots (for DM)
 NO_data = data.frame(cbind(data$NO_DM, data$NO_CWV))
 colnames(NO_data) = c("NO_DM", "NO_CWV")
+Kmax = 8
+SS_W = numeric(Kmax)
+for (K in 1:Kmax) {
+  km = kmeans(NO_data, K, iter.max = 50, nstart = 20)
+  SS_W[K] = km$tot.withinss
+}
+plot(1:Kmax, SS_W, type = "l", xlab = "K", ylab = "SS_W")
+## No obvious value of K but 4 is reasonable
 km_NO = kmeans(NO_data, 4, iter.max = 50, nstart = 20)
 pca_km_NO = prcomp(x = NO_data)
 plot(pca_km_NO$x[,1], pca_km_NO$x[,2], col = km_NO$cluster, pch = km_NO$cluster)
@@ -73,6 +112,12 @@ ggplot() +
   
 NE_data = data.frame(cbind(data$NE_DM, data$NE_CWV))
 colnames(NE_data) = c("NE_DM", "NE_CWV")
+for (K in 1:Kmax) {
+  km = kmeans(NE_data, K, iter.max = 50, nstart = 20)
+  SS_W[K] = km$tot.withinss
+}
+plot(1:Kmax, SS_W, type = "l", xlab = "K", ylab = "SS_W")
+## Potentially 2 clusters might be better for the NE data
 km_NE = kmeans(NE_data, 4, iter.max = 50, nstart = 20)
 pca_km_NE = prcomp(x = NE_data)
 plot(pca_km_NE$x[,1], pca_km_NE$x[,2], col = km_NE$cluster, pch = km_NE$cluster)
@@ -81,6 +126,54 @@ ggplot() +
   geom_point(data = NE_data, 
              mapping = aes(x = NE_CWV, y = NE_DM, colour = factor(cluster))) + 
   scale_colour_brewer(name = "Cluster", palette = "RdGy")
+
+## Plot each year over the top of each other
+data$Day_of_year = yday(data$Date)
+ggplot(data = data, aes(Day_of_year, NO_DM, group = factor(year(Date)),
+                        colour = factor(year(Date)))) + geom_line() +
+  scale_colour_brewer(palette = "Paired", name = "Year") + 
+  xlab("Day of the year")
+ggplot(data = data, aes(Day_of_year, NE_DM, group = factor(year(Date)),
+                        colour = factor(year(Date)))) + geom_line() +
+  scale_colour_brewer(palette = "Paired", name = "Year") + 
+  xlab("Day of the year")
+  
+## Plot the CSV and DM together
+ggplot(data = data, aes(x = Date)) + 
+  geom_line(mapping = aes(y = NO_DM, colour = "NO_DM")) + 
+  geom_line(mapping = aes(y = NO_CWV*2192.245, colour = "NO_CWV")) + 
+  scale_y_continuous(sec.axis = sec_axis(trans = ~./2192.245, name = "NO_CWV")) +
+  theme(legend.title = element_blank(), legend.position = "right") +
+  scale_color_manual(values = c("NO_CWV" = "darkgrey", "NO_DM" = "purple"))
+
+ggplot(data = data, aes(x = Date)) + 
+  geom_line(mapping = aes(y = NE_DM, color = "NE_DM")) + 
+  geom_line(mapping = aes(y = NE_CWV*2346.609, color = "NE_CWV")) + 
+  scale_y_continuous(sec.axis = sec_axis(trans = ~./2346.609, name = "NE_CWV")) +
+  theme(legend.title = element_blank(), legend.position = "right") +
+  scale_color_manual(values = c("NE_CWV" = "darkgrey", "NE_DM" = "purple"))
+
+## Look at correlation coefficients
+cor(data$NO_CWV, data$NO_DM)
+cor(data$NO_CWV, data$NO_DM, method = "spearman")
+cor(data$NE_CWV, data$NE_DM)
+cor(data$NE_CWV, data$NE_DM, method = "spearman")
+cor(data$NO_DM, data$NE_DM)
+cor(data$NO_DM, data$NE_DM, method = "spearman")
+
+cor.test(data$NO_CWV, data$NO_DM)
+cor.test(data$NO_CWV, data$NO_DM, method = "spearman")
+cor.test(data$NE_CWV, data$NE_DM)
+cor.test(data$NE_CWV, data$NE_DM, method = "spearman")
+cor.test(data$NO_DM, data$NE_DM)
+cor.test(data$NO_DM, data$NE_DM, method = "spearman")
+
+acf(data$NO_DM, lag.max = 40, main = "NO_DM acf")
+pacf(data$NO_DM, lag.max = 40)
+acf(data$NE_DM, lag.max = 40, main = "NE_DM acf")
+pacf(data$NE_DM, lag.max = 40)
+
+## End of included EDA
 
 ## Create some line plots of the data
 ggplot(data = data, mapping = aes(x = Date, y = NO_CWV)) + geom_line()
@@ -101,7 +194,6 @@ data$Type = NULL
 data$Bank.Holiday = replace_na(data$Bank.Holiday,"FALSE")
 
 ## Now can plot the bank holidays against other days
-ggplot(data = data) + geom_point(mapping = aes(x = NO_CWV, y = NE_CWV, colour = factor(Bank.Holiday)))
 ggplot(data = data) + 
   geom_point(mapping = aes(x = NO_DM, y = NE_DM, colour = factor(Bank.Holiday)), size = 0.4) + 
   scale_colour_discrete(limits = c("FALSE", "TRUE"), labels = c("Non-bank holiday", "Bank holiday"),
@@ -130,24 +222,24 @@ ggplot(data = data, mapping = aes(x = Date, y = NE_DM, colour = factor(Bank.Holi
 
 ## Change the black line to points for the bank holidays
 ggplot(data = data, mapping = aes(x = Date, y = NO_DM)) + 
-  geom_line(data = data[which(data$Bank.Holiday==FALSE),], aes(colour = factor(Bank.Holiday))) + 
+  geom_line(data = data[which(data$Bank.Holiday==FALSE),], aes(colour = factor(Bank.Holiday)), lwd = 0.35) + 
   geom_point(data = data[which(data$Bank.Holiday==TRUE),], aes(shape = factor(Bank.Holiday))) + 
   theme(legend.position = "bottom") + 
   labs(colour = "legend") + 
   scale_colour_discrete(name = "", breaks = c("TRUE", "FALSE"), 
                         labels = c("Bank holiday", "Non-bank holiday"),
-                        type = c("skyblue", "black")) +
+                        type = c("royalblue", "black")) +
   scale_shape_discrete(name = "", breaks = c("TRUE", "FALSE"), 
                         labels = c("Bank holiday", "Non-bank holiday"))
 
 ggplot(data = data, mapping = aes(x = Date, y = NE_DM)) + 
-  geom_line(data = data[which(data$Bank.Holiday==FALSE),], aes(colour = factor(Bank.Holiday))) + 
+  geom_line(data = data[which(data$Bank.Holiday==FALSE),], aes(colour = factor(Bank.Holiday)), lwd = 0.35) + 
   geom_point(data = data[which(data$Bank.Holiday==TRUE),], aes(shape = factor(Bank.Holiday))) + 
   theme(legend.position = "bottom") + 
   labs(colour = "legend") + 
   scale_colour_discrete(name = "", breaks = c("TRUE", "FALSE"), 
                         labels = c("Bank holiday", "Non-bank holiday"),
-                        type = c("skyblue", "black")) +
+                        type = c("royalblue", "black")) +
   scale_shape_discrete(name = "", breaks = c("TRUE", "FALSE"), 
                        labels = c("Bank holiday", "Non-bank holiday"))
 
@@ -166,10 +258,10 @@ ggplot(data = data) +
   scale_x_discrete(labels = c("FALSE" = "Non-bank holiday", "TRUE" = "Bank holiday"))
 
 ggplot(data = data) + geom_boxplot(mapping = aes(x = Bank.Holiday, y = NO_DM),
-                                   fill = c("skyblue", "darkgrey")) + xlab("") +
+                                   fill = c("royalblue", "darkgrey")) + xlab("") +
   scale_x_discrete(labels = c("FALSE" = "Non-bank holiday", "TRUE" = "Bank holiday"))
 ggplot(data = data) + geom_boxplot(mapping = aes(x = Bank.Holiday, y = NE_DM),
-                                   fill = c("skyblue", "darkgrey")) + xlab("") + 
+                                   fill = c("royalblue", "darkgrey")) + xlab("") + 
   scale_x_discrete(labels = c("FALSE" = "Non-bank holiday", "TRUE" = "Bank holiday"))
 
 
@@ -197,13 +289,10 @@ ggplot(data = data, mapping = aes(x = Date, y = NE_DM, colour = factor(day_type)
 
 ## Look at correlation coefficients
 cor(data$NO_CWV, data$NO_DM)
-#cor(data$NO_CWV, data$NO_DM, method = "kendall")
 cor(data$NO_CWV, data$NO_DM, method = "spearman")
 cor(data$NE_CWV, data$NE_DM)
-#cor(data$NE_CWV, data$NE_DM, method = "kendall")
 cor(data$NE_CWV, data$NE_DM, method = "spearman")
 cor(data$NO_DM, data$NE_DM)
-#cor(data$NO_DM, data$NE_DM, method = "kendall")
 cor(data$NO_DM, data$NE_DM, method = "spearman")
 
 cor.test(data$NO_CWV, data$NO_DM)
@@ -332,4 +421,34 @@ ggplot(data = data, aes(x = Date)) +
 
 data$NE_DM[1]/data$NE_CWV[1]
 data$NO_DM[1]/data$NO_CWV[1]
+
+## Attempts to plot each year on one another
+ggplot(data = data, aes(format(Date, format = "%m-%d"), NO_DM, group = factor(year(Date)),
+                        colour = factor(year(Date)))) + geom_line() +
+  scale_color_brewer(palette = "Paired")
+
+# Nulled just to remove from the dataset
+data$M_D = NULL
+data$Year = NULL
+data$M_D = format(data$Date, format = "%B-%d")
+data$Year = year(data$Date)
+
+ggplot(data = data, aes(M_D, NO_DM, group = factor(Year),
+                        colour = factor(Year))) + geom_line() + 
+  scale_color_brewer(palette = "Paired") + 
+  scale_x_date(breaks = "1 month")
+
+ggplot(data = data, aes(format(Date, format = "%m-%d"), NE_DM, group = factor(year(Date)),
+                        colour = factor(year(Date)))) + geom_line() +
+  scale_colour_brewer(palette = "Paired")
+
+dat1 = data.frame(date = seq(as.Date("2008-01-01"), as.Date("2008-12-31"), "1 day"),
+                  value = data$NO_DM[which(data$Year==2008)])
+dat2 = data.frame(date = seq(as.Date("2009-01-01"), as.Date("2009-12-31"), "1 day"),
+                  value = data$NO_DM[which(data$Year==2009)])
+ggplot(rbind(dat1, dat2), aes(format(date, format = "%m-%d"), value,
+                              group = factor(year(date)), 
+                              colour = factor(year(date)))) +
+  geom_line() + 
+  scale_x_discrete(labels = c(labels = c("date" = "month(date)")))
 
---
+++ function.R
@@ -0,0 +1,46 @@
+## Function
+y[i+1]  # Markov chain
+xi      # Transition matrix
+S0      # Initial distribution
+
+xi = matrix(c(0.9, 0.01, 0.01, 0.08, 0.04, 0.85, 0.01, 0.1, 
+                0.01, 0.01, 0.94, 0.04, 0.005, 0.005, 0.05, 0.94),  
+              nrow = 4, byrow = TRUE)
+
+markov_chain = function(xi, means, variances, length_seq, S0_distribution){
+  current_state # = what do I put here?
+  y = numeric(length_seq)
+  y[1] = # something for S0 distribution
+  for (i in 2:length_seq) {
+    current_state = sample(ncol(xi), size = 1, prob = xi[current_state,])
+    theta = rnorm(n = 1, mean = means[current_state], sd = sqrt(variances[current_state]))
+    y[i] = y[i-1] + theta
+  }
+}
+
+simulate_normal_hmm = function(xi, means, variances, length_seq, S0_distribution) {
+  r = length(means) # This is the number of states
+  s = numeric(length_seq+1) # The state sequence - note that the first element is S0 not S1
+  y = numeric(length_seq) # The observable sequence - note that the first element is Y1
+  s[1] = sample(r, 1, prob=S0_distribution)
+  if(isTRUE(all(rowSums(xi)==1)&&all(xi>=0))){  # Ensure the transition matrix is stochastic
+    for(i in 1:length_seq) {
+      # A) Sample s[i+1] - just like s[1] above except the prob argument in the sample function will
+      # be the s[i]-th row of xi
+      s[i+1] = sample(r, 1, prob = xi[s[i],])
+      # B) Sample y[i] - from a normal distribution where the mean and variance will be 
+      # the s[i+1]-th element of means and variances
+      y[i] = rnorm(n = 1, mean = means[s[i+1]], sd = sqrt(variances[s[i+1]]))
+    }
+    # Return a list containing both y and s
+    return(list(y,s))
+  }
+  else(return("Transition matrix is not stochastic"))
+}
+
+mu = c(1,10,30,60)
+sigma = c(0.5, 5, 8, 10)
+sim = simulate_normal_hmm(xi, mu, sigma, 500, c(0.8, 0.05, 0.07, 0.08))
+plot(sim[[1]], type = 'n')
+lines(sim[[1]])
+xi
